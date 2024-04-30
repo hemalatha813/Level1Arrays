6c713937917e7ad743c5cbb04a4f3ec27e0bfd32
hemalatha813 <167986059+hemalatha813@users.noreply.github.com>
2024-04-30 20:24:37 +0530

    Add files via upload

--- level1array.java
+++ level1array.java
@@ -8,51 +8,65 @@ public class level1array {
 		// TODO Auto-generated method stub
 		int arr[] ={1, 2, 3, 4, 5};
 		
-		//Creating and Initializing an Array
-     	int[] Array = new int[]{1, 2, 3 ,4 ,5};
+  //Creating and Initializing an Array
 		
+     	int[] myarray = new int[]{1, 2, 3 ,4 ,5};
         for (int i = 0; i <5; i++) {
-            System.out.println(arr[i]);
-  
+            System.out.println(myarray[i]);
+            
+        }  
+   //Accessing Elements of an Array
+        
+           for (int i=0;i<5;i++) {
+            if(i == 0) {
+            	System.out.println("The first element is: " + arr[i]);            	
+            }
+            if(i == arr.length - 1) {
+            	System.out.println("The fourth element is: " + arr[i]);             	
+            }
         }
         
-        //Accessing Elements of an Array
-       System.out.println("The first element is: " + arr[1]);
-       System.out.println("The fourth element is: " + arr[4]); 
-        
-        //Modifying Elements of an Array
+   //Modifying Elements of an Array
+           
         int[] arr3={1, 2, 3, 4, 5};
-        arr3[1]=10;
+        for(int i =0; i< arr3.length; i++) {
+        	if( i == 1 ) {
+        		arr3[i] = 10;
+        	}
+        }
         System.out.println(Arrays.toString(arr3));
         
-        // Finding the Length of an Array
+   // Finding the Length of an Array
+        
         System.out.println(arr.length);
         
-        //Looping through an Array
+   //Looping through an Array
+        
         for (int i = 0; i < arr.length; i++) {
             System.out.println("Element at index " + i + ": " + arr[i]);
         }
-        //Multi-dimensional Arrays
+        
+   //Multi-dimensional Arrays
+        
         int[][] array = { {1, 2}, {3, 4}, {5, 6} };
         System.out.println("Element at row 1 column 1: " + array[0][0]);
         
-        //Copying arrays
+   //Copying arrays
         
         int[] arr2 =new int[5];
         for(int i=0;i<arr2.length;i++) {
         	arr2[i]=arr[i];
-        	System.out.println(arr2[i]);
         }
+        System.out.println(Arrays.toString(arr2));
         
-        //Sorting Arrays
+   //Sorting Arrays
+        
         int[] a= {5, 3, 4, 1, 2};
         Arrays.sort(a);
         for(int i=0;i<a.length;i++){
-        	System.out.println(a[i]);
+        	System.out.println((a[i]));
         }
         
-        
-
 	}
 
 }
